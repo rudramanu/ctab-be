3d2e00c582c67914e9564c9f1650c87352917d23
Rudra <merudramanu@gmail.com>
2024-04-10 20:43:37 +0530

    added

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  "mysql://avnadmin:AVNS_LDNnL2j3Jr3UrjlS_MG@mysql-30b5284-merudramanu-1902.a.aivencloud.com:14844/ct-user"
+  `mysql://avnadmin:${process.env.MYSQL_PASSWORD}@mysql-30b5284-merudramanu-1902.a.aivencloud.com:14844/ct-user`
 );
 
 try {
